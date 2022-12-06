4c0e4b7271bd4289d5f602b5e7691b2211aa2140
xxx <25452644+bartoshevich@users.noreply.github.com>
2022-12-06 15:35:45 +0300

    feat: add audio success and error to  preload

--- src/assets/scripts/preload.js
+++ src/assets/scripts/preload.js
@@ -1,16 +1,25 @@
 import gameData from './data';
 
 async function preload() {
-for (let i = 1; i < gameData.length; i++) {
-  for (let j = 0; j < 6; j++) {
-    // const audio = new Audio();
-    // audio.src = gameData[i][j].audio;
-    // audio.load();
+  for (let i = 1; i < gameData.length; i++) {
+    for (let j = 0; j < 6; j++) {
+      // const audio = new Audio();
+      // audio.src = gameData[i][j].audio;
+      // audio.load();
 
-    const img = new Image();
-    img.src = gameData[i][j].image;
+      const img = new Image();
+      img.src = gameData[i][j].image;
+    }
   }
-}
+
+  let successSound = new Audio;
+  successSound.src = './success.mp3'
+  successSound.load();
+
+  let errorSound = new Audio;
+  errorSound.src = './error.mp3'
+  errorSound.play();
+
 }
 
 preload()
