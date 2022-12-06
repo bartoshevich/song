669ba630900a3b06d4b085dff05474248aee35d0
xxx <25452644+bartoshevich@users.noreply.github.com>
2022-12-06 16:21:28 +0300

    feat: add full preload

--- src/assets/scripts/preload.js
+++ src/assets/scripts/preload.js
@@ -3,9 +3,9 @@ import gameData from './data';
 async function preload() {
   for (let i = 1; i < gameData.length; i++) {
     for (let j = 0; j < 6; j++) {
-      // const audio = new Audio();
-      // audio.src = gameData[i][j].audio;
-      // audio.load();
+      const audio =  new Audio();
+      audio.src = gameData[i][j].audio;
+      audio.load();
 
       const img = new Image();
       img.src = gameData[i][j].image;
